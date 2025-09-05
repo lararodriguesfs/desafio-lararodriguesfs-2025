e5ecacfd4168bf58ce191f80d80d04504939db48
Lara Rodrigues <lararfonseca16@gmail.com>
2025-09-05 18:20:02 -0300

    encontraPessoas add validation and setup

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -13,11 +13,37 @@ class AbrigoAnimais {
   }
 
 
-encontrarPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
    var pessoa1 = this.parseEntrada(brinquedosPessoa1);
    var pessoa2 = this.parseEntrada(brinquedosPessoa2);
    var ordem = this.parseEntrada(ordemAnimais);
 
+
+if (this.temDuplicados(pessoa1) || this.temDuplicados(pessoa2)) {
+  return {erro: 'Brinquedo inválido'};
+}
+if (this.temDuplicados(ordem)) {
+  return {erro: 'Animal inválido'};
+}
+
+var resultado = [];
+var adotadosPessoa1 = 0;
+var adotadosPessoa2 = 0;
+
+for (var j = 0; j < ordem.length; j++) {
+  var animalNome = ordem[j];
+  var animal = this.animais[animalNome];
+
+  if(!animal) {
+    return {erro: 'Animal inválido'};
+  }
+
 }
 
 }
+
+}
+
+
+
+
