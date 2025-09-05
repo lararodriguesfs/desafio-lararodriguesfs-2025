3a5382d6882e7f0d8647d62a7c663ae7437b33c2
Lara Rodrigues <lararfonseca16@gmail.com>
2025-09-05 16:06:47 -0300

    Class AbrigoAnimais created

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,7 +1,16 @@
 class AbrigoAnimais {
+  constructor() {
 
-  encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    this.animais = {
+      'Rex': {tipo: 'cao', brinquedos: ['RATO', 'BOLA']},
+      'Mimi': {tipo: 'gato', brinquedos: ['RATO', 'LASER']},
+      'Fofo': {tipo: 'gato', brinquedos: ['BOLA', 'RATO', 'LASER']},
+      'Zero': {tipo: 'gato', brinquedos: ['RATO', 'BOLA']},
+      'Bola': {tipo: 'cao', brinquedos: ['CAIXA', 'NOVELO']},
+      'Bebe': {tipo: 'cao', brinquedos: ['LASER', 'RATO', 'BOLA']},
+      'Loco': {tipo: 'jabuti', brinquedos: ['SKATE', 'RATO']}
+    }
   }
 }
 
-export { AbrigoAnimais as AbrigoAnimais };
+export {AbrigoAnimais};
